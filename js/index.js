c9e549a76676bb69676c2f23ba269fda2b6593b1
entrmohit <67027313+entrmohit@users.noreply.github.com>
2020-06-29 00:26:25 +0530

    updating js file

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var r1=Math.floor(Math.random()*6)+1;
 var random1="images/dice"+r1+".png";
 var r2=Math.floor(Math.random()*6)+1;
-var random2="images/dice"+r2+".png";
+var random2="https://entrmohit.github.io/Conflict-Resolver/images/dice"+r2+".png";
 if(r1>r2){
   document.querySelector("h1").innerHTML="🚩 Player1 wins";
 }
